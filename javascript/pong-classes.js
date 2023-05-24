45ccee3bfbcdb18e112da2aa3c8ae739276e0956
Tai Pham <133684468+TaiYoBoi@users.noreply.github.com>
2023-05-24 12:13:59 -0500

    Update pong-classes.js

--- javascript/pong-classes.js
+++ javascript/pong-classes.js
@@ -16,7 +16,7 @@ export class Game {
     computerDifficultySpeed
   ) {
     this.size = { x: 600, y: 500 }; //container for entire game
-    this.padding = { x: 10, y: 10 }; //white space inside game border
+    this.padding = { x: 15, y: 15 }; //white space inside game border
     this.htmlElement = null; //element on HTML page
     this.mousePosition = { x: 0, y: 0 };
     this.mute = false; //default mute state
